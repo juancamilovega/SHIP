b17198170e744c6e6a7fd2d1d63c222f41334eed
camilo55 <camilo55@rogers.com>
2020-05-29 08:50:12 -0400

    Modified HLS maker to use specified speeds

--- src/tcl/hls_builder.tcl
+++ src/tcl/hls_builder.tcl
@@ -1,6 +1,9 @@
 set infile [open "Namefile.txt" r]
 set ip_name [gets $infile]
 close $infile
+set infile [open "Speed_grade.txt" r]
+set speed [gets $infile]
+close $infile
 set root_dir [file dirname [file dirname [file dirname [file normalize [info script]]]]]
 set module_name $ip_name
 cd $module_name
@@ -9,7 +12,7 @@ set_top $module_name
 add_files $root_dir/src/vhls/$module_name.cpp
 open_solution "solution1"
 set_part {xczu19eg-ffvc1760-2-i} -tool vivado
-create_clock -period 3.103 -name default
-config_rtl -reset all
+create_clock -period $speed -name default
+config_rtl -reset all -reset_level low
 csynth_design
 export_design -rtl verilog
